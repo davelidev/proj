7941de2cd10b9a3a75ba8475621aa32aa28385e5
davelidev <4515190+davelidev@users.noreply.github.com>
2026-05-10 13:57:04 -0700

    Batch 13: Add strategies 121-130

---
+++ quantconnect/Strategies/algos/algo_121.py
@@ -0,0 +1,12 @@
+class Algo121(BaseSubAlgo):
+    def initialize(self):
+        self.AddEquity("SPY")
+        self.AddEquity("QQQ")
+        self.AddEquity("IWM")
+
+    def update_targets(self):
+        self.targets = {
+            "SPY": 1/3,
+            "QQQ": 1/3,
+            "IWM": 1/3
+        }
